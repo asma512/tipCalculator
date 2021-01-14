9ad9780659499159f12b359cbdfa9ceac84e6ba4
Asma Sadia <asadia000@citymail.cuny.edu>
2021-01-13 20:38:23 -0500

    Initial Commit

---
+++ tips/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tips
+//
+//  Created by Asma Sadia on 1/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
